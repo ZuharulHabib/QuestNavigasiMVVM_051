612738365c77e27cc085e032ad915158f5e05410
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-11-21 17:50:30 +0700

    menambahkan data class mahasiswa alamat , email , dan no telpon untuk tampilan mobile

--- app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
@@ -3,5 +3,7 @@ package com.example.pertemuan7.model
 data class Mahasiswa(
     val nama: String = "",
     val gender: String = "",
-
+    val alamat: String = "",
+    val email: String = "",
+    val notelepon: String = ""
 )
