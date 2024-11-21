131e420e6c4c4df103f76b04b2eff94d74c9b31b
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-11-21 17:50:00 +0700

    menambahkan data class mahasiswa berisi nama gender

--- app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan7/model/Mahasiswa.kt
@@ -3,7 +3,5 @@ package com.example.pertemuan7.model
 data class Mahasiswa(
     val nama: String = "",
     val gender: String = "",
-    val alamat: String = "",
-    val email: String = "",
-    val notelepon: String = ""
+
 )
